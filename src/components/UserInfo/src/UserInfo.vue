3cb2c0a849ed0b037a0eb80dffef4fd53c7f5939
caiwp <caiwenpi@gmail.com>
2023-03-03 17:26:08 +0800

    fix: username

--- src/components/UserInfo/src/UserInfo.vue
+++ src/components/UserInfo/src/UserInfo.vue
@@ -2,11 +2,14 @@
 import { ElDropdown, ElDropdownMenu, ElDropdownItem, ElMessageBox } from 'element-plus'
 import { useI18n } from '@/hooks/web/useI18n'
 import { useCache } from '@/hooks/web/useCache'
+import { useAppStoreWithOut } from '@/store/modules/app'
 import { resetRouter } from '@/router'
 import { useRouter } from 'vue-router'
 import { loginOutApi } from '@/api/login'
 import { useDesign } from '@/hooks/web/useDesign'
 import { useTagsViewStore } from '@/store/modules/tagsView'
+
+const appStore = useAppStoreWithOut()
 
 const tagsViewStore = useTagsViewStore()
 
@@ -20,7 +23,10 @@ const { wsCache } = useCache()
 
 const { replace } = useRouter()
 
+const userInfo = wsCache.get(appStore.getUserInfo)
+
 const loginOut = () => {
+  console.log(userInfo)
   ElMessageBox.confirm(t('common.loginOutMessage'), t('common.reminder'), {
     confirmButtonText: t('common.ok'),
     cancelButtonText: t('common.cancel'),
@@ -47,7 +53,9 @@ const loginOut = () => {
         alt=""
         class="w-[calc(var(--logo-height)-25px)] rounded-[50%]"
       />
-      <span class="<lg:hidden text-14px pl-[5px] text-[var(--top-header-text-color)]">Archer</span>
+      <span class="<lg:hidden text-14px pl-[5px] text-[var(--top-header-text-color)]">{{
+        userInfo.username.toString()
+      }}</span>
     </div>
     <template #dropdown>
       <ElDropdownMenu>
